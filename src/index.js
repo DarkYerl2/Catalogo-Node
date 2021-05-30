98803e6a9f509b0ca3e85eeecd1eeef65a96855e
Daniel <darkkingyt55@gmail.com>
2021-05-30 11:53:06 -0500

    catalogo-terminado

--- src/index.js
+++ src/index.js
@@ -3,7 +3,7 @@ const express = require("express");
 const app = express();
 const path = require("path");
 const router = require("./routes");
-
+const port = process.env.PORT || 5000;
 
 
 // Settings

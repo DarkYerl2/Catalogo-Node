0038171ae1b9e9206c43c0a19959050f5c8a9656
Daniel <darkkingyt55@gmail.com>
2021-05-30 22:10:25 -0500

    Catalogo-Terminado

--- src/routes/index.js
+++ src/routes/index.js
@@ -2,27 +2,27 @@ const express = require("express");
 const router = express.Router();
 
 router.get("/", (req, res) => {
-  res.render("index", { title: "First Website" });
+  res.render("index", { title: "Catalogo" });
 });
 
 router.get("/juegos", (req, res) => {
-  res.render("juegos", { title: "First Website" });
+  res.render("juegos", { title: "Catalogo" });
 });
 
 router.get("/consolas", (req, res) => {
-  res.render("consolas", { title: "First Website" });
+  res.render("consolas", { title: "Catalogo" });
 });
 
 router.get("/camisetas", (req, res) => {
-  res.render("camisetas", { title: "First Website" });
+  res.render("camisetas", { title: "Catalogo" });
 });
 
 router.get("/figuras", (req, res) => {
-  res.render("figuras", { title: "First Website" });
+  res.render("figuras", { title: "Catalogo" });
 });
 
 router.get("/joyeria", (req, res) => {
-  res.render("joyeria", { title: "First Website" });
+  res.render("joyeria", { title: "Catalogo" });
 });
 
 
